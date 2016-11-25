ab07e0f311c49ad40d491476368ec92e217b53a5
jose <jmm.87@hotmail.com>
2016-11-24 21:12:19 -0300

    INTENTE agregar los botones de confirmacion dentro de la notificacion.

---
+++ app/src/main/java/com/example/jsolari/mvpauth0/NotificationReceiver.java
@@ -0,0 +1,25 @@
+package com.example.jsolari.mvpauth0;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+/**
+ * Created by v-monjos on 24-Nov-16.
+ */
+
+public class NotificationReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO Auto-generated method stub
+        String action = intent.getAction();
+        if (MyFirebaseMessagingService.OK_ACTION.equals(action)) {
+            Toast.makeText(context, "Si", Toast.LENGTH_SHORT).show();
+        }
+        else  if (MyFirebaseMessagingService.CANCEL_ACTION.equals(action)) {
+            Toast.makeText(context, "No", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
