b2fb6a27b2776d62d98ed54e48eea6a13c4a46c1
Nahuel Lema <lema.nahuel@gmail.com>
2017-10-23 19:28:08 -0300

    FIX, build

--- app/src/main/java/com/app/voluntariosos/mvpauth0/SplashActivity.java
+++ app/src/main/java/com/app/voluntariosos/mvpauth0/SplashActivity.java
@@ -4,14 +4,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class SplashActivity extends AppCompatActivity {
 
     private final int SPLASH_DISPLAY_LENGTH = 1500;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("Splash",  "splash");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
 
         new Handler().postDelayed(new Runnable(){
             @Override
